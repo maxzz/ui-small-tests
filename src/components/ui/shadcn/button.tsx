8426dbf0f448549f4c4d3d09541d8e7ed988c645
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-15 19:30:06 -0700

    WIP. components. 1. code review

--- src/components/ui/shadcn/button.tsx
+++ src/components/ui/shadcn/button.tsx
@@ -35,18 +35,8 @@ const buttonVariants = cva(
     }
 );
 
-function Button({
-    className,
-    variant,
-    size,
-    asChild = false,
-    ...props
-}: React.ComponentProps<"button"> &
-    VariantProps<typeof buttonVariants> & {
-        asChild?: boolean;
-    }) {
+function Button({ className, variant, size, asChild = false, ...props }: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants> & { asChild?: boolean; }) {
     const Comp = asChild ? Slot : "button";
-
     return (
         <Comp
             data-slot="button"
