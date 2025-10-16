a0ce1fddaa2921c8f5feac52843ab4d8e452e36a
Max Zakharzhevskiy <maxzz@msn.com>
2025-10-16 14:54:32 -0700

    WIP. tree. @kibo-ui/tree. code review

--- src/components/0-all/0-app.tsx
+++ src/components/0-all/0-app.tsx
@@ -2,14 +2,20 @@ import { Demo_Dashboard } from "../2-main/1-pages/1-dashboard";
 
 export function App() {
     return (
-        <div className="min-h-screen grid place-items-center">
-            <header>1</header>
-            <div className="p-4 w-full max-w-7xl grid grid-rows-2 gap-4">
+        <div className="min-h-screen grid grid-rows-[auto_1fr_auto] gap-4">
+            <header className="px-4 py-3 bg-gray-100">
+                1
+            </header>
+
+            <div className="place-self-center p-4 w-full max-w-7xl grid grid-rows-2 place-items-center gap-4">
                 <div className="bg-green-500">123</div>
 
                 <Demo_Dashboard />
             </div>
-            <footer>3</footer>
+
+            <footer className="px-4 py-3 bg-gray-100">
+                3
+            </footer>
         </div>
     );
 }
