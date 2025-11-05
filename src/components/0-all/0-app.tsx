1bcca48fec9934a726995fc582b32999d66e6683
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-04 18:28:04 -0800

    WIP. components-under-cursor. switch left view to sidebar. 2

--- src/components/0-all/0-app.tsx
+++ src/components/0-all/0-app.tsx
@@ -2,20 +2,25 @@ import { Section1_Header } from "../1-header";
 import { Section3_Footer } from "../3-footer";
 import { LeftList } from "../2-main/1-left-list";
 import { RenderDemo } from "./1-render-demo";
+import { SidebarProvider, SidebarTrigger } from "../ui/shadcn/sidebar";
 
 export function App() {
     return (
         <div className="h-screen grid grid-rows-[auto_1fr_auto]">
-            <Section1_Header className="1bg-gray-100" />
-
-            <div className="grid grid-cols-[250px_1fr] gap-4 overflow-hidden">
+            <SidebarProvider>
                 <LeftList />
-                <RenderDemo />
-            </div>
 
-            <footer className="bg-gray-100">
-                <Section3_Footer />
-            </footer>
+                <main className="size-full overflow-hidden 1min-h-0">
+                    <div className="size-full grid grid-rows-[auto_1fr_auto]">
+                        <Section1_Header className="1bg-gray-100" />
+                        <RenderDemo />
+                        <footer className="bg-gray-100">
+                            <Section3_Footer />
+                        </footer>
+
+                    </div>
+                </main>
+            </SidebarProvider>
         </div>
     );
 }
--- src/components/1-header/1-header.tsx
+++ src/components/1-header/1-header.tsx
@@ -1,12 +1,17 @@
 import { type HTMLAttributes } from "react";
 import { classNames } from "@/utils";
 import { HeaderToolbar } from "./2-header-toolbar";
+import { SidebarTrigger } from "../ui/shadcn/sidebar";
 
 export function Section1_Header({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
     return (
         <header className={classNames(sectionClasses, className)} {...rest}>
-            <div className="pb-1 text-[.65rem]">
-                ... the time is gone, the song is over, thought I'd something more to say.
+            <div className="flex items-center">
+                <SidebarTrigger />
+
+                <div className="text-[.5rem] hover:scale-150 transition-transform origin-left">
+                    ... the time is gone, the song is over, thought I'd something more to say.
+                </div>
             </div>
 
             <HeaderToolbar />
