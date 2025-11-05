249d5c0c4a68f652de80f9685c9b16bf6dc11d6d
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-05 15:45:33 -0800

    WIP. header-toolbar. integrate formatHoverStackTooltip()

--- src/components/2-main/1-left-list/3-group-tw-props.tsx
+++ src/components/2-main/1-left-list/3-group-tw-props.tsx
@@ -1,31 +1,28 @@
-"use client";
-import { useCallback } from "react";
+import { useMemo } from "react";
+import { useAtomValue } from "jotai";
 import { useSnapshot } from "valtio";
-import { appSettings, type NodeId } from "@/store/0-local-storage";
-import {
-    Sidebar,
-    SidebarContent,
-    SidebarGroup,
-    SidebarGroupLabel,
-    SidebarGroupContent,
-    SidebarMenu,
-    SidebarMenuItem,
-    SidebarMenuButton,
-} from "@/components/ui/shadcn/sidebar";
+import { appSettings } from "@/store/0-local-storage";
+import { SidebarGroup, SidebarGroupLabel, SidebarGroupContent } from "@/components/ui/shadcn/sidebar";
 import { ScrollArea } from "@/components/ui/shadcn/scroll-area";
+import { hoverStackAtom } from "../2-pages/8-mouse-tracker/7-hover-stack-atoms";
+import { formatHoverStackTooltip } from "../2-pages/8-mouse-tracker/8-utils-format";
 
 export function GroupTwProps() {
     const { leftTree } = useSnapshot(appSettings.appUi);
+    const hoverStack = useAtomValue(hoverStackAtom);
+    const tooltipContent = useMemo(() => formatHoverStackTooltip(hoverStack), [hoverStack]);
 
     return (
-        <SidebarGroup className="border-t border-border h-1/2">
+        <SidebarGroup className="border-t border-border h-1/2 overflow-hidden flex flex-col">
             <SidebarGroupLabel>
                 Tailwind CSS props
             </SidebarGroupLabel>
 
             <SidebarGroupContent>
                 <ScrollArea className="size-full">
-                    <div className="">123</div>
+                    <div className="max-h-dvh whitespace-pre text-left p-0">
+                        {tooltipContent}
+                    </div>
                 </ScrollArea>
             </SidebarGroupContent>
         </SidebarGroup>
