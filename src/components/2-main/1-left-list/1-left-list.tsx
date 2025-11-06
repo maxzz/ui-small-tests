83c96564082141a1cc0e833489d283ff25aaae93
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-06 13:55:39 -0800

    WIP. resizable. code clean up

--- src/components/2-main/1-left-list/1-left-list.tsx
+++ src/components/2-main/1-left-list/1-left-list.tsx
@@ -7,17 +7,6 @@ import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/componen
 import { classNames } from "@/utils";
 import { panelsStorage } from "@/store/0-local-storage";
 
-// export function LeftList() {
-//     return (
-//         <Sidebar>
-//             <SidebarContent className="flex flex-col justify-between">
-//                 <GroupDemos />
-//                 <GroupTwProps/>
-//             </SidebarContent>
-//         </Sidebar>
-//     );
-// }
-
 export function LeftList() {
     return (
         <Sidebar>
@@ -25,48 +14,18 @@ export function LeftList() {
 
                 <ResizablePanelGroup className="1md:min-w-[450px] 1min-h-[200px] max-w-md rounded-lg border" direction="vertical" autoSaveId="main" storage={panelsStorage}>
                     <ResizablePanel defaultSize={25}>
-                        {/* <div className="flex h-full items-center justify-center p-6">
-                            <span className="font-semibold">Header</span>
-                        </div> */}
                         <GroupDemos />
                     </ResizablePanel>
 
-                    {/* <ResizableHandle className="w-8 hover:size-8 active:size-8 1bg-blue-800 hover:bg-green-500 active:bg-red-500" /> */}
-                    
-                    {/* <ResizableHandle className="my-[3px] h-0.5! 1pb-4 1items-end z-20" tabIndex={-1} withHandle /> */}
-
-                    <ResizableHandle className="my-[3px] 1pb-4 1items-end z-20" tabIndex={-1} withHandle>
-                        <div className="flex items-center gap-1">
-                            {/* <button className={toysArrowClasses} onClick={() => togglePanels(refA, refB, true)}>
-                                <IconChevronLeft />
-                            </button> */}
-
-                            <ResizableHandleToys className={toysMiddleClasses} />
-
-                            {/* <button className={toysArrowClasses} onClick={() => togglePanels(refA, refB, false)}>
-                                <IconChevronLeft className="rotate-180" />
-                            </button> */}
-                        </div>
-                    </ResizableHandle>
+                    <ResizableHandle className="my-[3px] h-0.5! hover:bg-sky-500 active:bg-sky-500 transition-colors  delay-150 z-20" tabIndex={-1} withHandle />
 
                     <ResizablePanel defaultSize={75}>
-                        {/* <div className="flex h-full items-center justify-center p-6">
-                            <span className="font-semibold">Content</span>
-                        </div> */}
                         <GroupTwProps />
                     </ResizablePanel>
                 </ResizablePanelGroup>
 
             </SidebarContent>
         </Sidebar>
-    );
-}
-
-function ResizableHandleToys({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
-    return (
-        <div className={classNames("w-3 h-4 rounded-sm border bg-border flex items-center justify-center z-10", className)} {...rest}>
-            <IconRadix_DragHandleDots2 className="h-2.5 w-2.5 text-red-500" />
-        </div>
     );
 }
 
