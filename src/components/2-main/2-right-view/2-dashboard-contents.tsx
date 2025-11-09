9e6ddeba19b2ec0d4408e3d6ab242af7036a9f1f
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-08 18:58:28 -0800

    WIP. blocks. layout update. 6.

--- src/components/2-main/2-right-view/2-dashboard-contents.tsx
+++ src/components/2-main/2-right-view/2-dashboard-contents.tsx
@@ -3,27 +3,33 @@ import data from "../3-pages/1-demo-contents/2-dashboard/8-data.json";
 
 export function DashboardContents() {
     return (
-        <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                <SectionCards />
+        <div className="@container/main flex-1 flex flex-col gap-2">
+            <div className="py-4 md:py-6 flex flex-col gap-4 md:gap-6">
+
+                <div className={cardsClasses}>
+                    <TotalRevenueCard />
+                    <NewCustomersCard />
+                    <ActiveAccountsCard />
+                    <GrowthRateCard />
+                </div>
 
                 <div className="px-4 lg:px-6">
                     <ChartAreaInteractive />
                 </div>
-                
+
                 <DataTable data={data} />
             </div>
         </div>
     );
 }
 
-function SectionCards() {
-    return (
-        <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
-            <TotalRevenueCard />
-            <NewCustomersCard />
-            <ActiveAccountsCard />
-            <GrowthRateCard />
-        </div>
-    );
-}
+const cardsClasses = "\
+px-4 lg:px-6 \
+\
+dark:*:data-[slot=card]:bg-card \
+*:data-[slot=card]:bg-linear-to-t \
+*:data-[slot=card]:from-primary/5 \
+*:data-[slot=card]:to-card \
+*:data-[slot=card]:shadow-xs \
+\
+grid grid-cols-1 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 gap-4";
