bdfe78ca4aec535ad02e9714c3a710a064f65dc6
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-09 16:41:30 -0800

    WIP. tw-values-to-show. more strict checks in filterColorClasses(). 5

--- src/components/2-main/1-left-list/3-group-tw-props.tsx
+++ src/components/2-main/1-left-list/3-group-tw-props.tsx
@@ -5,7 +5,7 @@ import { appSettings } from "@/store/0-local-storage";
 import { SidebarGroup, SidebarGroupLabel, SidebarGroupContent } from "@/components/ui/shadcn/sidebar";
 import { ScrollArea } from "@/components/ui/shadcn/scroll-area";
 import { hoverStackAtom } from "../3-pages/8-mouse-tracker/7-hover-stack-atoms";
-import { formatHoverStackTooltip } from "../3-pages/8-mouse-tracker/8-utils-format";
+import { formatHoverStackTooltip } from "../3-pages/8-mouse-tracker/4-utils-format";
 // import { TestTargetWindowPositionWReset } from "@/components/ui/local/3-test-target-position-w-reset";
 
 export function GroupTwProps() {
