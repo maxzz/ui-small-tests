5e3b892a013782ee4d76c85d6e6b3834cdf8718a
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-13 18:08:12 -0800

    WIP. select-theme. add tooltip to show current colors

--- src/components/1-header/4-header-toolbar-select-theme.tsx
+++ src/components/1-header/4-header-toolbar-select-theme.tsx
@@ -3,6 +3,7 @@ import { useAtomValue } from "jotai";
 import { useSnapshot } from "valtio";
 import { appSettings } from "@/store/0-local-storage";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/shadcn/select";
+import { Tooltip, TooltipTrigger, TooltipContent } from "../ui/shadcn/shadcn-modified/tooltip";
 import { getPresetThemeStyles, themeNamesAtom } from "@/store/2-apply-theme";
 
 export function SelectTheme({ className, ...rest }: HTMLAttributes<HTMLDivElement>) {
@@ -10,22 +11,33 @@ export function SelectTheme({ className, ...rest }: HTMLAttributes<HTMLDivElemen
     const themeNames = useAtomValue(themeNamesAtom);
 
     return (
-        <Select defaultValue={themePreseetName} onValueChange={(name) => appSettings.appUi.themePreseetName = name}>
-            <SelectTrigger className="px-2 h-7! text-xs rounded-sm" title="primary, accent, secondary, border">
-                <SelectValue placeholder="Select" />
-            </SelectTrigger>
+        <Tooltip>
+            <Select defaultValue={themePreseetName} onValueChange={(name) => appSettings.appUi.themePreseetName = name}>
+                <TooltipTrigger asChild>
+                    <SelectTrigger className="px-2 h-7! text-xs rounded-sm" title="primary, accent, secondary, border">
+                        <SelectValue placeholder="Select" />
+                    </SelectTrigger>
+                </TooltipTrigger>
 
-            <SelectContent align="end" alignOffset={-4}>
-                {themeNames.map(
-                    (name) => (
-                        <SelectItem className="text-xs [&_span]:last:w-full [&_span]:last:block [&_span]:last:justify-between" value={name} key={name}>
-                            {name}
-                            <ThemeColors presetName={name} mode="light" />
-                        </SelectItem>
-                    )
-                )}
-            </SelectContent>
-        </Select>
+                <SelectContent align="end" alignOffset={-4}>
+                    {themeNames.map(
+                        (name) => (
+                            <SelectItem className="text-xs [&_span]:last:w-full [&_span]:last:block [&_span]:last:justify-between" value={name} key={name}>
+                                {name}
+                                <ThemeColors presetName={name} mode="light" />
+                            </SelectItem>
+                        )
+                    )}
+                </SelectContent>
+            </Select>
+
+            <TooltipContent side="bottom" sideOffset={5}>
+                <div className="flex flex-col gap-1">
+                    <div className="text-xs font-semibold">{themePreseetName}</div>
+                    <ThemeColors presetName={themePreseetName} mode="light" />
+                </div>
+            </TooltipContent>
+        </Tooltip>
     );
 }
 
