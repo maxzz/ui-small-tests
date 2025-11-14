3e23fae3b414b1ac4784bb9bb121840194e9ed33
Max Zakharzhevskiy <maxzz@msn.com>
2025-11-13 18:41:33 -0800

    WIP. select-theme. ThemeTooltipContent change items order

--- src/components/1-header/5-header-toolbar-tooltip-contents.tsx
+++ src/components/1-header/5-header-toolbar-tooltip-contents.tsx
@@ -5,13 +5,18 @@ import { ColorBox } from "./6-header-toolbar-select-color-box";
 export function ThemeTooltipContent({ presetName }: { presetName: string; }) {
     const mode = "light";
     const styles = getPresetThemeStyles(presetName)[mode];
+    const entries = Object.entries(styles);
+    const numRows = Math.ceil(entries.length / 2);
 
     return (
         <TooltipContent side="bottom" sideOffset={5} className="bg-muted border-foreground/20 border shadow">
             <div className="flex flex-col gap-1.5 max-w-xs">
                 <div className="text-xs font-semibold border-b border-foreground/20 pb-1">{presetName}</div>
-                <div className="grid grid-cols-2 gap-x-3 gap-y-1 text-[10px]">
-                    {Object.entries(styles).map(
+                <div 
+                    className="grid grid-flow-col gap-x-3 gap-y-1 text-[10px]"
+                    style={{ gridTemplateRows: `repeat(${numRows}, minmax(0, 1fr))` }}
+                >
+                    {entries.map(
                         ([key, value]) => (
                             <ThemeStyleItem key={key} name={key} value={value as string} />
                         )
