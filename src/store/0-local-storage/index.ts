aa1e93e2c03aad5d2794642e3e42e40057740f48
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-23 19:13:04 -0800

    WIP. demos. extract motion-examples type and definitions

--- src/store/0-local-storage/index.ts
+++ src/store/0-local-storage/index.ts
@@ -1,3 +1,4 @@
 export * from "./1-load-save";
 export * from "./8-resizable-storage";
 export * from "./9-types";
+export * from "../../components/2-main/3-pages/4-motion-examples/9-motion-example-types";
