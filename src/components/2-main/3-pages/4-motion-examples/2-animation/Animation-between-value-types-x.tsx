4325dfe4dad222c91a18c1a034e5e270856cedea
Max Zakharzhevskiy <maxzz@msn.com>
2025-12-26 15:30:03 -0800

    WIP. demos. add BezierCurveEditor. 1

--- src/components/2-main/3-pages/4-motion-examples/2-animation/Animation-between-value-types-x.tsx
+++ src/components/2-main/3-pages/4-motion-examples/2-animation/Animation-between-value-types-x.tsx
@@ -6,22 +6,39 @@ import { motion, useCycle } from "motion/react";
  */
 export function AnimationBetweenValueTypesXDemo() {
     const [x, cycleX] = useCycle<string | number>(0, "calc(3 * var(--width))");
+    const [y, cycleY] = useCycle<string | number>(0, "50px", "100px", "calc(3 * var(--width))");
 
     return (
-        <div className="px-4">
-            <div className="mb-4">Click to cycle back and forth X: {x}</div>
+        <div className="p-4 bg-sky-100 text-xs">
+            <div>Cycles between number and string.</div>
+            <div>Click to cycle back and forth X1: {x}</div>
+            <div className="mb-4">Click to cycle back and forth X2: {y}</div>
+
             <motion.div
                 initial={false}
                 animate={{ x }}
                 transition={{ duration: .5, ease: () => 0.5 }}
                 style={{
-                    width: 100,
-                    height: 100,
                     "--width": "50px",
                 }}
-                className="1size-24 rounded-2xl bg-sky-700"
+                className="m-4 size-24 rounded-2xl bg-sky-200 text-xl flex items-center justify-center"
                 onClick={() => cycleX()}
-            />
+            >
+                X1
+            </motion.div>
+
+            <motion.div
+                initial={false}
+                animate={{ x: y }}
+                transition={{ duration: .5, ease: () => 0.5 }}
+                style={{
+                    "--width": "50px",
+                }}
+                className="m-4 size-24 rounded-2xl bg-sky-200 text-xl flex items-center justify-center"
+                onClick={() => cycleY()}
+            >
+                X2
+            </motion.div>
         </div>
     );
 }
